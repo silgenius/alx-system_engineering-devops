a32105d04c4d8dfef2b8a61b335a9aeb37873113
silgenius <olutademartin@gmail.com>
2024-05-07 17:23:07 -0700

    Fix error in code

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 from8 = ARGV[0].scan(/from:\+?(\d+|\w+)/).join
-to = ARGV[0].scan(/from:\+?(\d+|\w+)/).join
+to = ARGV[0].scan(/to:\+?(\d+|\w+)/).join
 flag = ARGV[0].scan(/flags:(-1:0:-1:-?[0-1]:-1?)/).join
 puts "#{from8},#{to},#{flag}"
