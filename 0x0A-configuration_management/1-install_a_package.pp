7c20d8b1148d33a827c9ad7e11928109acea57d3
silgenius <olutademartin@gmail.com>
2024-06-28 17:33:35 -0700

    Fix code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet to install flask from pip3
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
 }
