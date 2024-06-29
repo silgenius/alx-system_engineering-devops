3c78a7adbf89883b3e553ca62f4d98ed1034e5f5
silgenius <olutademartin@gmail.com>
2024-06-28 17:55:20 -0700

    Solution to task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,8 @@ exec { 'install flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/bin',
 }
+
+exec { 'install Werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => '/usr/bin',
+}
