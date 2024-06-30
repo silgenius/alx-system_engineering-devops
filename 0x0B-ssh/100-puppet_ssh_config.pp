100c42cfda24f3bd9f5055eda3ed62535006c845
silgenius <olutademartin@gmail.com>
2024-06-30 11:16:54 -0700

    Apply different approach to code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
 # Puppet to make changes to our configuration file
 
 
-exec { 'Declare identity file':
-  command => 'sed -i \'s|IdentityFile.*|IdentityFile ~/.ssh/school|g\' /root/.ssh/school',
-  path    => ['/usr/bin', '/usr/local/bin', '/opt/puppetlabs/bin'],
-}
+$file_content = "Host *
+\tIdentityFile ~/.ssh/school
+\tPasswordAuthentication no
+\tSendEnv LANG LC_*
+\tHashKnownHosts yes
+\tGSSAPIAuthentication yes
+"
 
-exec { 'Turn off passwd auth':
-  command => 'sed -i \'s|PasswordAuthentication.*|PasswordAuthentication no|g\' /root/.ssh/school',
-  path    => ['/usr/bin', '/usr/local/bin', '/opt/puppetlabs/bin'],
+file { '/etc/ssh/ssh_config':
+path    => '/etc/ssh/ssh_config',
+ensure  => present,
+content => $file_content,
 }
