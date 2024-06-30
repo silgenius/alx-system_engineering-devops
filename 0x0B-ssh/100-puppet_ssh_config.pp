b8d889d60e7a6572cd6c177cfa939a3c58063585
silgenius <olutademartin@gmail.com>
2024-06-30 10:25:25 -0700

    Refactor code to pass all unit test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,13 @@
 # Puppet to make changes to our configuration file
 
-package { 'puppetlabs-stdlib':
-  ensure   => 'installed',
-  provider => 'puppet_gem',
-}
-
 file_line {'ensure SSH client configuration must be configured to use the private key ~/.ssh/school':
-  path  => '~/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
+  path  => '/root/.ssh/config',
+  line  => 'IdentityFile /root/.ssh/school',
   match => '^\s*IdentityFile.*'
 }
 
 file_line {'ensure  SSH client configuration must be configured to refuse to authenticate using a password':
-  path  => '~/.ssh/config',
+  path  => '/root/.ssh/config',
   line  => 'PasswordAuthentication no',
   match => '^\s*PasswordAuthentication.*'
 }
