b9ff589f82b2c5092b06f531e0e63f7c1f5e2bb2
silgenius <olutademartin@gmail.com>
2024-07-08 16:42:29 -0700

    Implement solution to task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,48 @@
+# configuring your server with Puppet
+
+$file_content="server {
+\tlisten 80 default_server;
+\tlisten [::]:80 default_server;
+\troot /var/www/html;
+
+\t# Add index.php to the list if you are using PHP
+\tindex index.html index.htm index.nginx-debian.html;
+
+\tserver_name _;
+
+ \tlocation /redirect_me {
+ \t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+\t}
+}"
+
+$config _header = "http {\n\n\tadd_header X-Served-By \"$(hostname)\";"
+
+exec {'apt_update':
+  command => 'apt-get update',
+  path    => ['usr/bin', 'usr/sbin'],
+}
+
+package {'nginx':
+  ensure => 'installed',
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => "Hello World!\n",
+}
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => $file_content,
+}
+
+file_line {'Include header':
+  ensure => '/etc/nginx/nginx.conf',
+  line   => $config_header
+  match  => 'http {'
+}
+
+service {'nginx':
+  ensure => 'running',
+  enable => true,
+}
