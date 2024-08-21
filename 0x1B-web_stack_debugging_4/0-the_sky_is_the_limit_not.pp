0c2785d06da3e435d77fa60cde9cc65e5a01b2a5
silgenius <olutademartin@gmail.com>
2024-08-20 20:29:12 -0700

    Update file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # configure nginx to accept more request
 
 exec {'fix--for-nginx':
- command => 'sed -i \'s/ULIMIT="-n 4096"/ULIMIT="-n 4096"/\' /etc/default/nginx && sudo service nginx restart',
-  path   => ['/bin/', '/usr/bin/', '/usr/sbin/'],
+  command => 'sed -i \'s/ULIMIT="-n [0-9]*"/ULIMIT="-n 4096"/\' /etc/default/nginx && sudo service nginx restart',
+  path    => ['/bin/', '/usr/bin/', '/usr/sbin/'],
 }
